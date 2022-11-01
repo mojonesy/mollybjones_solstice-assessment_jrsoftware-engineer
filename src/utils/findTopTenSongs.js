dd1e5d2b50383c8cc28d743730e939fd1020fe86
Molly <mojonesycreative@gmail.com>
2022-11-01 12:16:14 -0400

    import json for testing

--- src/utils/findTopTenSongs.js
+++ src/utils/findTopTenSongs.js
@@ -1,3 +1,9 @@
-export default function findTopTenSongs(history) {
-    
+import listenHistory from "../data/listen_history.json" assert { type: "json" };
+
+function findTopTenSongs(history) {
+    console.log("hello");
 }
+
+findTopTenSongs();
+
+// export default findTopTenSongs;
