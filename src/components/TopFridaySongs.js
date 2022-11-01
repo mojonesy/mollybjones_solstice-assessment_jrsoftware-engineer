d375b9804f4112ac8f02fb7aa5a8c43496af8936
Molly <mojonesycreative@gmail.com>
2022-11-01 17:08:01 -0400

    import card components

---
+++ src/components/TopFridaySongs.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function TopFridaySongs() {
+    return (
+        <div className="card">
+            <h2 className="card-title">
+                Friday's After-work Hits
+            </h2>
+            <button className="btn">
+                Click Here
+            </button>
+        </div>
+    )
+}
+
+export default TopFridaySongs;
---
+++ src/components/TopTenSongs.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function TopTenSongs() {
+    return (
+        <div className="card">
+            <h2 className="card-title">
+                Top 10 Most Played
+            </h2>
+            <button className="btn">
+                Click Here
+            </button>
+        </div>
+    )
+}
+
+export default TopTenSongs;
