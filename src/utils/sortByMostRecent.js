ea8d3ba285e6799daddbd6779f1e0e6b2cf12ad6
Molly <mojonesycreative@gmail.com>
2022-11-01 12:16:23 -0400

    import json for testing

--- src/utils/sortByMostRecent.js
+++ src/utils/sortByMostRecent.js
@@ -1,3 +1,9 @@
-export default function sortByMostRecent(history) {
-    
+import listenHistory from "../data/listen_history.json" assert { type: "json" };
+
+function sortByMostRecent(history) {
+    console.log("hi");
 }
+
+sortByMostRecent();
+
+// export default sortByMostRecent;
