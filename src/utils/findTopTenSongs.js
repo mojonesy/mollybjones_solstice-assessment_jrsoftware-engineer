f1ce3329d8ca879935133e732bf7bbb7e2e0d64a
Molly <mojonesycreative@gmail.com>
2022-11-01 13:23:37 -0400

    added helper function to reduce list to song/artist only

--- src/utils/findTopTenSongs.js
+++ src/utils/findTopTenSongs.js
@@ -1,9 +1,41 @@
 import listenHistory from "../data/listen_history.json" assert { type: "json" };
 
-function findTopTenSongs(history) {
-    console.log("hello");
+export default function findTopTenSongs(history) {
+    if(history.length === 0) return null;
+
 }
 
-findTopTenSongs();
 
-// export default findTopTenSongs;
+
+
+/**
+ * @returns an array of objects containing songs and artists only
+ * lists artist as "n/a" if undefined
+ */
+function getArtistsAndTitles(history) {
+    let result = [];
+
+    // "watch" represents an individual listen in the json array
+    history.forEach(watch => {  
+        let song = watch.title.substring(8);                // remove "Watched" string from title              
+        if (watch.subtitles !== undefined) {
+            let artist = watch.subtitles[0].name.slice(0, -8);  // remove " - Topic" string from subtitles.name                        
+            result.push({
+                song: song,
+                artist: artist
+            });
+        } else {
+            result.push({
+                artist: "n/a",
+                song: song
+            });
+        }
+    });
+
+    return result;
+}
+
+
+
+
+findTopTenSongs(listenHistory);
