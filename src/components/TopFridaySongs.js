f6dccc91460ef0e331c979dc6c7e312e5938bd1d
Molly <mojonesycreative@gmail.com>
2022-11-01 22:38:40 -0400

    Friday component mapped

--- src/components/TopFridaySongs.js
+++ src/components/TopFridaySongs.js
@@ -1,14 +1,35 @@
 import React from "react";
 
-function TopFridaySongs() {
+function TopFridaySongs({ handleTopFriday, topFriday, error }) {
     return (
         <div className="card">
             <h2 className="card-title">
-                Friday's After-work Hits
+              Friday's After-work Hits
             </h2>
-            <button className="btn">
-                Click Here
+            <p className="card-subtitle">
+              Top 10 songs played after 4pm
+            </p>
+            <button 
+              className="btn"
+              onClick={handleTopFriday}
+            >
+              Click Here
             </button>
+
+            { topFriday && 
+              <div className="list">
+                {topFriday.map((watch) => {
+                  return (
+                    <ul className="list-items" key={watch.song}>
+                      <li className="item">
+                        <span>{watch.song} </span> by <span>{watch.artist}</span> was played <span>{watch.count} </span> times
+                      </li>
+                    </ul>
+                    );
+                })}
+              </div>
+            }
+            { error && <p>An error occurred...</p> }
         </div>
     )
 }
