2fef4c2827c9ecf745da9fc65fe907dc0b80e3dd
Molly <mojonesycreative@gmail.com>
2022-11-01 22:39:35 -0400

    handlers, state, props finished

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TopTenSongs from './components/TopTenSongs';
 import TopFridaySongs from './components/TopFridaySongs';
 import './App.css';
+import findTopTenSongs from './utils/findTopTenSongs';
+import sortByFridayAfterFour from './utils/sortByFridayAfterFour';
+import history from "./data/listen_history.json";
 
 function App() {
+  const [topTen, setTopTen] = useState([]);
+  const [topFriday, setTopFriday] = useState([]);
+  const [error, setError] = useState(null);
+
+  // top ten songs handler
+  const handleTopTen = (e) => {
+    e.preventDefault();
+    
+    const getData = () => {
+      return Promise.resolve(findTopTenSongs(history));
+    }
+    getData()
+      .then(data => setTopTen(data))
+      .then(() => console.log(topTen))
+      .catch(err => setError(err));
+  }
+
+  // top ten friday after 4pm songs handler
+  const handleTopFriday = (e) => {
+    e.preventDefault();
+
+    const getData = () => {
+      return Promise.resolve(sortByFridayAfterFour(history));
+    }
+    getData()
+      .then(data => setTopFriday(data))
+      .then(() => console.log(topFriday))
+      .catch(err => setError(err));
+  }
 
   return (
-    <div className="App">
+    <div className="App container">
       <h1 className="title">
         <div className="logo">
           <img 
@@ -16,8 +48,16 @@ function App() {
       </h1>
 
       <div className="cards">
-        <TopTenSongs />
-        <TopFridaySongs />
+        <TopTenSongs 
+          handleTopTen={handleTopTen} 
+          topTen={topTen}
+          error={error}
+        />
+        <TopFridaySongs 
+          handleTopFriday={handleTopFriday}
+          topFriday={topFriday}
+          error={error}
+        />
       </div>
     </div>
   );
