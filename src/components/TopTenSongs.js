02c472bc9931e146b6024aaa0d28c9e1fbfd8014
Molly <mojonesycreative@gmail.com>
2022-11-01 22:38:53 -0400

    topTen component mapped

--- src/components/TopTenSongs.js
+++ src/components/TopTenSongs.js
@@ -1,16 +1,37 @@
 import React from "react";
 
-function TopTenSongs() {
+function TopTenSongs({ handleTopTen, topTen, error }) {
     return (
         <div className="card">
             <h2 className="card-title">
-                Top 10 Most Played
+              Top 10 Most Played
             </h2>
-            <button className="btn">
-                Click Here
+            <p className="card-subtitle">
+              All-time
+            </p>
+            <button 
+              className="btn"
+              onClick={handleTopTen}
+            >
+              Click Here
             </button>
+
+            { topTen && 
+              <div className="list">
+                {topTen.map((watch) => {
+                  return (
+                    <ul className="list-items" key={watch.song}>
+                      <li className="item">
+                        <span>{watch.song} </span> by <span>{watch.artist}</span> was played <span>{watch.count} </span> times
+                      </li>
+                    </ul>
+                    );
+                })}
+              </div>
+            }
+            { error && <p>An error occurred...</p> }
         </div>
-    )
+    );
 }
 
 export default TopTenSongs;
